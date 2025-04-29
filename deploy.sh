fae7a9bad6141d33d44a0750e841c36945b1dc55
rajacloud99 <rajasekhar.cloud99@outlook.com>
2025-04-29 16:32:29 +0530

    databrick configs & default workspace

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ DATABRICKS_WORKSPACE_PATH="/Shared/sample-assets"
 PROFILE_NAME="<PROFILE_NAME>"
 
 # Upload the files to Databricks
-databricks workspace import_dir ./notebooks $DATABRICKS_WORKSPACE_PATH --profile $PROFILE_NAME --overwrite
+databricks workspace import_dir ./src $DATABRICKS_WORKSPACE_PATH --overwrite
 
 echo "Deployment completed successfully."

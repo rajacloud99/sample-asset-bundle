829ae18d04cc623b7dfa12c324fbd7d661670c25
rajacloud99 <rajasekhar.cloud99@outlook.com>
2025-04-30 14:18:12 +0530

    addition

---
+++ src/python/addition.py
@@ -0,0 +1,11 @@
+# Take input from the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Take input from the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum of the two numbers
+sum_result = num1 + num2
+
+# Display the result
+print("The sum of the two numbers is:", sum_result)

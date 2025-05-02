45044abbd867cb2e0aa8d4b99e9e6d54589e90b8
rajacloud99 <rajasekhar.cloud99@outlook.com>
2025-05-02 13:14:16 +0530

    bundle changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Variables
-DATABRICKS_WORKSPACE_PATH="/Shared/sample-assets"
-PROFILE_NAME="<PROFILE_NAME>"
+# # Variables
+# DATABRICKS_WORKSPACE_PATH="/Shared/sample-assets"
+# PROFILE_NAME="<PROFILE_NAME>"
 
-# Upload the files to Databricks
-databricks workspace import_dir ./src $DATABRICKS_WORKSPACE_PATH --overwrite
+# # Upload the files to Databricks
+# databricks workspace import_dir ./src $DATABRICKS_WORKSPACE_PATH --overwrite
 
-echo "Deployment completed successfully."
+# echo "Deployment completed successfully."
